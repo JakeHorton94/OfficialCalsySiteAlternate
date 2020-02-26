b69cf0fcf12945e7a347482bbaf657c0695fb084
Jake <jake.horton94@hotmail.co.uk>
2020-02-26 13:35:15 +0000

    typo change in fleetwork js

--- src/pages/fleetWork.js
+++ src/pages/fleetWork.js
@@ -1,5 +1,5 @@
 import React from "react"
-import './fleetWork.scss'
+import './fleetwork.scss'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Link } from 'gatsby'
