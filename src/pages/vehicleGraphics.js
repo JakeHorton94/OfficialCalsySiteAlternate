2a9ecc54b7e53a3c51e6d52e132540dffed8f7c7
Jake <jake.horton94@hotmail.co.uk>
2020-03-02 14:34:11 +0000

    Rough layout of individual service page added

--- src/pages/vehicleGraphics.js
+++ src/pages/vehicleGraphics.js
@@ -1,5 +1,5 @@
 import React from "react"
-import './bespokeStickers.scss'
+import './vehicleGraphics.scss'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import blogStyles from './blog.module.scss'
@@ -27,7 +27,24 @@ const VehicleGraphicsPage = () => {
 
         return (
             <Layout>
-                <h1> Vehicle Graphics </h1>
+              <img className="serviceLogo" src="https://i.imgur.com/4Qarum6.png"></img>
+              <div className="section1">
+                <p className="serviceIntro">Vehicle & van graphics are the best way to promote your business and your services while on the move. First impressions are very important and we will ensure that your vehicle or van perfectly represents you and your business.</p>
+                <p><span className="blueDot">•</span><span className="redDot">•</span><span className="yellowDot">•</span></p>
+                <h2 className="threeSteps"> VEHICLE GRAPHICS IN 3 EASY STEPS</h2>
+                <div className="point">
+                  <span className="listNumber1">1.</span><p>Our in-house design team will work with you to produce a free no-obligation design and quotation.</p>
+                </div>
+                <div className="point">
+                  <span className="listNumber2">2.</span><p>Your vehicle will be booked in with us at a time that is convenient for you. We even offer local fully insured vehicle collection and return for a flat rate of £29.99 + VAT.</p>
+                </div>
+                <div className="point">
+                  <span className="listNumber3">3.</span><p>Your vehicle is ready to collect or be returned to you fully branded, all artwork is released and you leave happy. Safe in the knowledge that the work carried has been carried out to the highest standard and is covered by unrivalled aftercare.</p>
+                </div>
+                </div>
+
+              <div className="section1">
+                <h2> Take a look at some of our work </h2>
                 <ol className={blogStyles.posts}>
                     {data.allMarkdownRemark.edges.map((edge) => {
                         return (
@@ -41,6 +58,8 @@ const VehicleGraphicsPage = () => {
                         )
                     })}
                 </ol>
+                </div>
+                
             </Layout>
         )
   }
